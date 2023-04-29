0118512f0a94677344b228d87274401af68f4796
Tiago Laim <tiago.laim.rodrigues@gmail.com>
2023-04-29 23:50:49 +0100

    Versão: 0.2.3.01 (Early Access)
    Não tenho 100% certeza, como ainda não fiz um teste completo, mas já dá para editar os ingredientes.
    Ao criar isto, lembrei me que pode haver problemas quando se apaga um ingrediente e se volta a adicionar um.

--- CookApp/lib/edit_recipe.dart
+++ CookApp/lib/edit_recipe.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable, no_leading_underscores_for_local_identifiers
+
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,11 +14,13 @@ Future<bool> showConfirmationDialog(BuildContext context) async {
     builder: (BuildContext context) {
       return Theme(
         data: ThemeData(
-            brightness: Brightness.dark,
-            textTheme: const TextTheme(
-                titleMedium: TextStyle(
+          brightness: Brightness.dark,
+          textTheme: const TextTheme(
+            titleMedium: TextStyle(
               color: Colors.red,
-            ))),
+            ),
+          ),
+        ),
         child: CupertinoAlertDialog(
           title: const Text(
             'Sair?',
@@ -57,7 +61,7 @@ void editingDialog(BuildContext context, Recipe toEditR) {
   String desc = toEditR.descricao;
   List<String>? ings = toEditR.ingredientes;
   List<String>? ingsT = toEditR.ingTipo;
-  List<double>? ingsQ = toEditR.ingQuant;
+  List<double> ingsQ = toEditR.ingQuant!;
   List<String>? procs = toEditR.procedimento;
   double? tempo = toEditR.tempo;
   double? porcs = toEditR.porcoes;
@@ -66,6 +70,17 @@ void editingDialog(BuildContext context, Recipe toEditR) {
 
   final _nameKey = GlobalKey<FormFieldState>();
   final _descKey = GlobalKey<FormFieldState>();
+  final _ingsKey = GlobalKey<FormFieldState>();
+
+  final TextEditingController _ingsController = TextEditingController();
+  final TextEditingController _quantController = TextEditingController();
+
+  String? ingsR;
+
+  List<TextEditingController> hintControllers = List.generate(
+    toEditR.ingQuant!.length,
+    (_) => TextEditingController(text: toEditR.ingQuant![_].toStringAsFixed(0)),
+  );
 
   showDialog(
     context: context,
@@ -128,6 +143,7 @@ void editingDialog(BuildContext context, Recipe toEditR) {
                                                   BorderRadius.circular(8),
                                               child: Image.file(
                                                 File(foto!),
+                                                height: 100,
                                               ),
                                             ),
                                             Positioned(
@@ -212,7 +228,197 @@ void editingDialog(BuildContext context, Recipe toEditR) {
                               ],
                             ),
                           ),
-                          //!Gestão da foto principal
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: ings!.isEmpty
+                                  ? const Center(
+                                      child: Text(
+                                        'Nenhum ingrediente',
+                                        style: TextStyle(
+                                          fontSize: 22,
+                                        ),
+                                      ),
+                                    )
+                                  : ListView.builder(
+                                      shrinkWrap: true,
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      itemCount: ings.length,
+                                      itemBuilder:
+                                          (BuildContext context, int index) {
+                                        return Padding(
+                                          padding: const EdgeInsets.only(
+                                              bottom: 8, left: 16, right: 16),
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              color: Colors.grey[200],
+                                            ),
+                                            child: ListTile(
+                                              onLongPress: () {
+                                                setState(() {
+                                                  ings.remove(ings[index]);
+                                                  ingsT.remove(ingsT[index]);
+                                                  ingsQ.remove(ingsQ[index]);
+                                                });
+                                              },
+                                              title: Text(
+                                                ings[index],
+                                                style: const TextStyle(
+                                                    fontSize: 18),
+                                              ),
+                                              trailing: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceBetween,
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  Semantics(
+                                                    label: 'quantidade',
+                                                    child: SizedBox(
+                                                      width: 32,
+                                                      height: 44,
+                                                      child: TextField(
+                                                        controller:
+                                                            hintControllers[
+                                                                index],
+                                                        style: const TextStyle(
+                                                          color: Colors.black54,
+                                                          fontSize: 16,
+                                                        ),
+                                                        textAlign:
+                                                            TextAlign.center,
+                                                        onChanged: (value) {
+                                                          final newValue =
+                                                              double.tryParse(
+                                                                  value);
+                                                          if (newValue !=
+                                                              null) {
+                                                            setState(() {
+                                                              ingsQ[index] =
+                                                                  newValue;
+                                                            });
+                                                          } else {
+                                                            ingsQ[index] = 0;
+                                                          }
+                                                        },
+                                                        keyboardType:
+                                                            TextInputType
+                                                                .number,
+                                                        decoration:
+                                                            const InputDecoration(
+                                                          border:
+                                                              InputBorder.none,
+                                                          hintText: '0',
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 10,
+                                                  ),
+                                                  DropdownButton<String>(
+                                                    value: ingsT![index],
+                                                    style: const TextStyle(
+                                                      color: Colors.black54,
+                                                      fontSize: 20,
+                                                    ),
+                                                    items: <String>[
+                                                      'Kg',
+                                                      'g',
+                                                      'L',
+                                                      'mL',
+                                                      'unid.',
+                                                      'colh.',
+                                                      'chav.',
+                                                    ].map<
+                                                        DropdownMenuItem<
+                                                            String>>(
+                                                      (String value) {
+                                                        return DropdownMenuItem<
+                                                            String>(
+                                                          value: value,
+                                                          child: Text(value),
+                                                        );
+                                                      },
+                                                    ).toList(),
+                                                    onChanged:
+                                                        (String? newValue) {
+                                                      setState(() {
+                                                        ingsT[index] =
+                                                            newValue!;
+                                                      });
+                                                    },
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                right: 16, left: 16, top: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  flex: 3,
+                                  child: TextFormField(
+                                    key: _ingsKey,
+                                    controller: _ingsController,
+                                    decoration: const InputDecoration(
+                                      labelText: 'Ingredientes',
+                                      hintText: 'Insira um ingrediente',
+                                      border: OutlineInputBorder(),
+                                    ),
+                                    onChanged: (value) {
+                                      ingsR = value;
+                                    },
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  flex: 0,
+                                  child: IconButton(
+                                    tooltip: 'Adicionar',
+                                    splashRadius: 35,
+                                    iconSize: 50,
+                                    splashColor: Colors.black12,
+                                    color: Colors.orange,
+                                    icon: const Icon(Icons.add),
+                                    onPressed: () {
+                                      setState(() {
+                                        if (ingsR != null) {
+                                          hintControllers.add(
+                                            TextEditingController(text: '0'),
+                                          );
+                                          //* Parte principal
+                                          ings.add(ingsR!);
+                                          _ingsController.text = '';
+                                          ingsR = '';
+
+                                          //* Parte das opções
+                                          ingsT!.add('g');
+                                          ingsQ.add(0);
+                                        }
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -235,11 +441,12 @@ void editingDialog(BuildContext context, Recipe toEditR) {
                         ),
                         ElevatedButton(
                           onPressed: () {
-                            _nameKey.currentState!.validate();
-                            _descKey.currentState!.validate();
-
-                            bool validate = _nameKey.currentState!.validate() &&
-                                _descKey.currentState!.validate();
+                            if (_nameKey.currentState != null) {
+                              _nameKey.currentState!.validate();
+                            }
+                            if (_descKey.currentState != null) {
+                              _descKey.currentState!.validate();
+                            }
 
                             if (nome.isNotEmpty && desc.isNotEmpty) {
                               editRecipeById(
@@ -249,9 +456,9 @@ void editingDialog(BuildContext context, Recipe toEditR) {
                                   foto: foto,
                                   nome: nome,
                                   descricao: desc,
-                                  ingredientes: toEditR.ingredientes,
-                                  ingTipo: toEditR.ingTipo,
-                                  ingQuant: toEditR.ingQuant,
+                                  ingredientes: ings,
+                                  ingTipo: ingsT,
+                                  ingQuant: ingsQ,
                                   procedimento: toEditR.procedimento,
                                   tempo: toEditR.tempo,
                                   porcoes: toEditR.porcoes,
