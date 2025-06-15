8aa9dc3836467db864882d0781a457bacb0ae6a9
Tiago Laim <tiago.laim.rodrigues@gmail.com>
2025-06-15 15:08:19 +0200

    Version Change 1.7.2.1

--- cookapp/test/widget_test.dart
+++ cookapp/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 //! Keep if not used
 
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cookapp/main.dart';
