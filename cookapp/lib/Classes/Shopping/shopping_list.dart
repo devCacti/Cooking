ff3c2878464c0e26854f466d7dd0af46a490f5c1
Tiago Laim <tiago.laim.rodrigues@gmail.com>
2025-07-30 19:44:13 +0100

    Removed Unused Import

--- cookapp/lib/Classes/Shopping/shopping_list.dart
+++ cookapp/lib/Classes/Shopping/shopping_list.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:cookapp/Classes/file.dart';
 import 'package:flutter/material.dart';
